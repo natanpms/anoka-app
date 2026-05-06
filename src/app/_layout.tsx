f9b1dbde0b4eeba8a1d7c135816f475177ec687f
Natan <natanportoms@outlook.com>
2026-05-06 12:29:39 -0300

    first commit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,5 +3,6 @@ import "../global.css";
 import { Slot } from "expo-router";
 
 export default function Layout() {
+
   return <Slot />;
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,31 +1,56 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { ActivityIndicator, Image, Text, View } from "react-native";
+import { SafeAreaView } from "../components/SafeAreaView";
 
 export default function App() {
   return (
-    <View className="flex-1 bg-white dark:bg-black items-center justify-center px-8">
-      {/* Heading */}
-      <Text className="text-4xl font-extrabold text-gray-800 dark:text-white mb-3 tracking-tight">
-        🚀 Welcome
-      </Text>
+    <SafeAreaView className="flex-1 bg-secondary-foreground">
+      <StatusBar
+        style="inverted"
+        backgroundColor="#020617"
+        translucent={false}
+      />
 
-      {/* Subheading */}
-      <Text className="text-xl dark:text-white text-gray-700 mb-8 text-center leading-relaxed">
-        Build beautiful apps with{" "}
-        <Text className="text-blue-500 font-semibold">
-          Expo (Router) + Uniwind 🔥
+      <View className="py-6 w-full flex-row items-center justify-center">
+        <Text
+          className="text-[10px] text-center tracking-widest text-secondary font-semibold"
+        >
+          Um pequeno passo para um grande futuro...
         </Text>
-      </Text>
+      </View>
+      <View className="flex-1 items-center justify-around px-8 py-12">
 
-      {/* Instruction text */}
-      <Text className="text-base text-gray-600 dark:text-white text-center max-w-sm">
-        Start customizing your app by editing{" "}
-        <Text className="font-semibold text-gray-800 dark:text-white">
-          app/index.tsx
-        </Text>
-      </Text>
+        <View className="items-center gap-8">
+          <View className="h-36 w-36 items-center justify-center overflow-hidden rounded-full bg-white">
+            <Image
+              className="h-24 w-24 rounded-full"
+              source={require("../assets/images/logo.jpg")}
+            />
+          </View>
 
-      <StatusBar style="dark" />
-    </View>
+          <View className=" justify-center items-center">
+            <Text
+              className="text-4xl text-primary font-bold"
+            >
+              Anoka
+            </Text>
+
+            <Text className="max-w-72 text-center text-base leading-6 text-secondary font-light">
+              Simples e eficiente.
+            </Text>
+
+          </View>
+        </View>
+
+        <View className="w-full items-center gap-4">
+          <ActivityIndicator color="#16a34a" size="small" />
+          <Text
+            className="text-sm text-secondary font-medium"
+          >
+            Preparando experiência
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView >
   );
 }
